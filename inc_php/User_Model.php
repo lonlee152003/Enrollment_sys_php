afbbdac6c5abbd416983f939d41a9e1cb3a0061f
lonlee152003 <72242445+lonlee152003@users.noreply.github.com>
2020-10-22 18:42:12 +0800

    v1.00.05

---
+++ inc_php/User_Model.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of User_Model
+ *
+ * @author Clemente-PC
+ */
+class User_Model {
+    private $first_name_um;
+    private $middle_name_um;
+    private $last_name_um;
+    private $id_number_um;
+    private $email_address_um;
+    private $middle_name_um;
+    private $middle_name_um;
+    private $middle_name_um;
+    private $middle_name_um;
+    private $middle_name_um;
+    private $middle_name_um;
+    private $middle_name_um;
+    private $middle_name_um;
+}
